a531b6e94e5b743bd9d61f33219a3a5de6472b31
hc8756 <hc8756@rit.edu>
2024-10-12 23:14:32 -0400

    made fixes to algorithm and added readme

--- scanner.py
+++ scanner.py
@@ -1,11 +1,11 @@
 temp = "" # holds strings while they are being evaluated
 tokens=[]
+keywords=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Style", "CONT", "heading_color", "rose_pink"]
 
 def transition_from_S0 (current_char):
     global temp
     global tokens
-    temp="" #always reset temp string from S0
-    if current_char.isalpha():
+    if current_char.isalpha() or current_char=='_':
         temp+=current_char
         return 'KEYWORD_OR_LITERAL'
     if current_char.isdigit(): #even if it turns out to be string later, try finding time
@@ -13,9 +13,11 @@ def transition_from_S0 (current_char):
         return 'TIME'
     elif current_char in '{"}";':
         tokens.append(('Delimiter', current_char))
+        temp = ""
         return 'S0'
     elif current_char in '-=#':
         tokens.append(('Operator', current_char))
+        temp = ""
         return 'S0'
     elif current_char.isspace():
         return 'S0' # just ignore
@@ -25,20 +27,27 @@ def transition_from_S0 (current_char):
 def transition_from_S1(current_char):
     global temp
     global tokens
-    if current_char.isalpha() or current_char.isdigit() or current_char.isspace():
+    if current_char.isalpha() or current_char.isdigit() or current_char.isspace() or current_char=='_':
         temp += current_char
-        if  temp in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Style", "CONT", "heading_color", "rose_pink"]:
-            tokens.append(('Keyword', temp))
-            return 'S0'
+        for keyword in keywords:
+            if keyword in temp: #if keyword is identified, add split temp into literal (may be nothing) and keyword and add both tokens
+                keyword_start = temp.find(keyword)
+                if(keyword_start>0):
+                    tokens.append(("Literal", temp[:keyword_start]))
+                tokens.append(("Keyword", temp[ keyword_start:]))
+                temp = ""
+                return 'S0'
         else:
             return 'KEYWORD_OR_LITERAL'
     elif current_char in '{"}";':
         tokens.append(('Literal', temp))
         tokens.append(('Delimiter', current_char))
+        temp = ""
         return 'S0'
     elif current_char in '-=#':
         tokens.append(('Literal', temp))
         tokens.append(('Operator', current_char))
+        temp = ""
         return 'S0'
     else:
         return 'ERROR'
@@ -46,22 +55,25 @@ def transition_from_S1(current_char):
 def transition_from_S2 (current_char):
     global temp
     global tokens
-    if current_char.isalpha():
+    if current_char.isalpha() or current_char.isspace() or current_char=='_':
         temp += current_char # we thought it might be time, but it's a keyword or literal now
         return 'KEYWORD_OR_LITERAL'
     if current_char.isdigit() or current_char==':': # right now any <=4-char combination of numbers and ':'
         temp += current_char
         if len(temp) >= 5:
             tokens.append(('Time', temp))
+            temp = ""
             return 'S0'
         return 'TIME'
     elif current_char in '{"}";':
         tokens.append(('Time', temp))
         tokens.append(('Delimiter', current_char))
+        temp = ""
         return 'S0'
     elif current_char in '-=#':
         tokens.append(('Time', temp))
         tokens.append(('Operator', current_char))
+        temp = ""
         return 'S0'
     else:
         return 'ERROR'
@@ -83,7 +95,8 @@ def lexer(input_program):
             print('Unrecognized character. We are at position ' + str(current_position))
             break
         current_position += 1
-
+    if temp: #anything left in temp at this point would be literal
+        tokens.append(('Literal', temp))
     return 0
 
 input_program = input("Please enter something: ")
