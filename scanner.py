de90174e774a9979b7eed8834a1ddeb982adab77
hc8756 <hc8756@rit.edu>
2024-11-09 21:22:10 -0500

    basic recursive loop for non-terminal A in

--- scanner.py
+++ scanner.py
@@ -1,6 +1,9 @@
 temp = "" # holds strings while they are being evaluated
 tokens=[]
 keywords=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Style", "CONT", "heading_color", "rose_pink"]
+days= ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+elements= ["heading_color"]
+values= ["rose_pink"]
 
 def transition_from_S0 (current_char):
     global temp
@@ -11,7 +14,7 @@ def transition_from_S0 (current_char):
     if current_char.isdigit(): #even if it turns out to be string later, try finding time
         temp += current_char
         return 'TIME'
-    elif current_char in '{"}";':
+    elif current_char in '{};':
         tokens.append(('Delimiter', current_char))
         temp = ""
         return 'S0'
@@ -87,7 +90,7 @@ def lexer(input_program):
     tokens = []
     current_state = 'S0'
     current_position=0
-    while current_position < len(input_program): #for each char of the program
+    while current_position < len(input_program): # for each char of the program
         current_char = input_program[current_position]
         if current_state == 'S0':
             current_state=transition_from_S0(current_char)
@@ -96,18 +99,39 @@ def lexer(input_program):
         elif current_state == 'TIME': # S2 in function naming convention
             current_state=transition_from_S2(current_char)
         elif current_state == 'ERROR':
-            print('Unrecognized character. We are at position ' + str(current_position))
+            print('Unrecognized character at position ' + str(current_position))
             break
         current_position += 1
-    if temp: #anything left in temp at this point would be literal
+    if temp: # anything left in temp at this point would be literal
         tokens.append(('Literal', temp))
     return 0
 
 # Assignment 2 code starts here
 # TODO: implement class for lexer like you did for parser
+# TODO: change language to not recognize quotations, they serve no function
+# TODO: move parser to its own script
+# TODO: create a main script that imports scanner and parser
+
+# Boilerplate code for creating and representing nodes of tree
+class ASTNode:
+    def __init__(self, node_type, value=""):
+        self.node_type = node_type
+        self.value = value
+        self.children = []
+
+    def add_child(self, child):
+        if child is not None:
+            self.children.append(child)
+
+    def __repr__(self, level=0):
+        indent = "  " * level
+        repr_str = f"{indent}{self.node_type}: {self.value}\n"
+        for child in self.children:
+            repr_str += child.__repr__(level + 1)
+        return repr_str
 
 class Parser:
-    def __init__(self, tokens, curr_pos):
+    def __init__(self, tokens, curr_pos=0):
         self.tokens=tokens
         self.curr_pos=0
 
@@ -117,24 +141,48 @@ class Parser:
         return None
 
     def parse(self):
+        if self.current_token() is None: # no input no tree
+            return None
+        return self.parse_S() # if input return
+
+    def parse_S(self):
+        ast = ASTNode("S")
+        ast.add_child(self.parse_A())  # Parse block A
+        #ast.add_child(self.parse_B())  # Parse block B
+        return ast
+
+    def parse_A(self):
+        ast = ASTNode("A")
+        if self.current_token() is None:
+            return ast
+        if self.current_token()[1] in days:
+            ast.add_child(ASTNode('WD',self.current_token()[1]))
+            self.curr_pos += 1
+            if self.current_token() is not None and self.current_token()[1]=="{":
+                ast.add_child(ASTNode(self.current_token()[0], self.current_token()[1]))
+                self.curr_pos += 1
+                if self.current_token() is not None and self.current_token()[0]=='Literal':
+                    #ast.add_child(parse_SCH())
+                    self.curr_pos += 1
+                    if self.current_token() is not None and self.current_token()[1]=="}":
+                        ast.add_child(ASTNode(self.current_token()[0], self.current_token()[1]))
+                        self.curr_pos += 1
+                        ast.add_child(self.parse_A())
+                        return ast
+                    else:
+                        self.syntax_error()
+                else:
+                    self.syntax_error()
+            else:
+                self.syntax_error()
+        else:
+            self.syntax_error()
+
+    def syntax_error(self):
+        print("Syntactic error at " + str(self.curr_pos))
+        for token in self.tokens:
+            print("<" + token[0] + ", \"" + token[1] + "\">\n")
         return None
-
-# Boilerplate code for creating and representing nodes of tree
-class ASTNode:
-    def __init__(self, node_type, value=None):
-        self.node_type = node_type
-        self.value = value
-        self.children = []
-
-    def add_child(self, child):
-        self.children.append(child)
-
-    def __repr__(self, level=0):
-        indent = "  " * level
-        if self.children:
-            children_repr = "\n".join([child.__repr__(level+1) for child in self.children])
-            return f"{indent}{self.node_type}: {self.value}\n{children_repr}"
-        return f"{indent}{self.node_type}: {self.value}"
 
 input_program = input("Please enter something: ")
 lexer(input_program)
